bd39ef64182bd4ae56c5aab3d73b7eb4b5f24d49
pedro <pedro.rinpiz@educa.jcyl.es>
2024-04-29 12:04:04 +0200

    primer commit

---
+++ pruebaGitHub/src/pruebaGitHub/PruebaGit.java
@@ -0,0 +1,14 @@
+package pruebaGitHub;
+
+public class PruebaGit 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Esta es una prueba con Git");
+
+		System.out.println("modificación del archivo");
+		
+	}
+
+}
